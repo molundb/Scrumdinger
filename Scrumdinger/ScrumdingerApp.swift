e1bbdd91bd5f9e0ee287f6bf67f9772728651969
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-02-22 15:58:25 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Martin Lundberg on 22.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
